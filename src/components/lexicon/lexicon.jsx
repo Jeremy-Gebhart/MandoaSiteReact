20d650d530da1abd28d19aebe7f3720386e756f1
Gebhart <jbgebha@4004160mac.caresource.corp>
2018-09-12 13:02:18 -0400

    Added the rest of the vocabulary, and tied it into the app

--- src/components/lexicon/lexicon.jsx
+++ src/components/lexicon/lexicon.jsx
@@ -23,7 +23,7 @@ export default class Lexicon extends React.Component {
             });
         }
 
-        let url = "data/mandoa_sorted/" + datasetName + '_vocab.json';
+        let url = "data/mandoa_sorted/" + datasetName + '.json';
         Axios.get(url)
             .then((response) => {                
                 console.log(response.data);
@@ -40,9 +40,33 @@ export default class Lexicon extends React.Component {
         return (
             <section className="lexiconComponent">
                 <h1>Lexicon</h1>
-                <article className="linkList">
-                    <LinkButton linkText="A" clickEvent={() => this.getWordList('a')} />
-                    <LinkButton linkText="B" clickEvent={() => this.getWordList('b')} />
+                <article className="linkList">                    
+                    <LinkButton linkText="A" clickEvent={() => this.getWordList('a_vocab')} />
+                    <LinkButton linkText="B" clickEvent={() => this.getWordList('b_vocab')} />
+                    <LinkButton linkText="C" clickEvent={() => this.getWordList('c_vocab')} />
+                    <LinkButton linkText="D" clickEvent={() => this.getWordList('d_vocab')} />
+                    <LinkButton linkText="E" clickEvent={() => this.getWordList('e_vocab')} />
+                    <LinkButton linkText="F" clickEvent={() => this.getWordList('f_vocab')} />
+                    <LinkButton linkText="G" clickEvent={() => this.getWordList('g_vocab')} />
+                    <LinkButton linkText="H" clickEvent={() => this.getWordList('h_vocab')} />
+                    <LinkButton linkText="I" clickEvent={() => this.getWordList('i_vocab')} />
+                    <LinkButton linkText="J" clickEvent={() => this.getWordList('j_vocab')} />
+                    <LinkButton linkText="K" clickEvent={() => this.getWordList('k_vocab')} />
+                    <LinkButton linkText="L" clickEvent={() => this.getWordList('l_vocab')} />
+                    <LinkButton linkText="M" clickEvent={() => this.getWordList('m_vocab')} />
+                    <LinkButton linkText="N" clickEvent={() => this.getWordList('n_vocab')} />
+                    <LinkButton linkText="O" clickEvent={() => this.getWordList('o_vocab')} />
+                    <LinkButton linkText="P" clickEvent={() => this.getWordList('p_vocab')} />
+                    <LinkButton linkText="R" clickEvent={() => this.getWordList('r_vocab')} />
+                    <LinkButton linkText="S" clickEvent={() => this.getWordList('s_vocab')} />
+                    <LinkButton linkText="T" clickEvent={() => this.getWordList('t_vocab')} />
+                    <LinkButton linkText="U" clickEvent={() => this.getWordList('u_vocab')} />
+                    <LinkButton linkText="V" clickEvent={() => this.getWordList('v_vocab')} />
+                    <LinkButton linkText="W" clickEvent={() => this.getWordList('w_vocab')} />
+                    <LinkButton linkText="Y" clickEvent={() => this.getWordList('y_vocab')} />
+                    <LinkButton linkText="Numbers" clickEvent={() => this.getWordList('numbers')} />
+                    <LinkButton linkText="Phrases" clickEvent={() => this.getWordList('phrases')} />
+                    <LinkButton linkText="Prepositions" clickEvent={() => this.getWordList('prepositions')} />
                 </article>
                 {!this.state.isGreetingHidden && <Greeting />}
                 {!this.state.isTableHidden && <VocabDisplay wordList={this.state.wordList} />}
@@ -98,5 +122,24 @@ const Greeting = (props) => {
                 </table>
             </section>
         );  
+    }
+}
+
+class ExpandableWindow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <section className="expandable">
+                <input id="collapsible" className="toggle" type="checkbox" />
+                <section className="collapsible-content">
+                    <section className="content">
+
+                    </section>
+                </section>
+                <label htmlFor="collapsible" className="lbl-toggle">Click here to see more options</label>
+            </section>
+        );
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
                 })
             },
             {
-                test: /\.ttf$/,
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
